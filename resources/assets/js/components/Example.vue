0755605b45ffb448b16fd11f501303428aa8c664
“GabrielMoreno” <gabrieljmorenot@gmail.com>
2017-05-21 10:03:57 -0400

    Compila JS y CSS con Laravel Mix.

--- resources/assets/js/components/Example.vue
+++ resources/assets/js/components/Example.vue
@@ -3,10 +3,11 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Example Component</div>
+                    <div class="panel-heading">Bienvenido {{ name }}</div>
 
                     <div class="panel-body">
-                        I'm an example component!
+                        Escribe tu nombre:
+                        <input v-model="name" class="form-control">
                     </div>
                 </div>
             </div>
@@ -16,6 +17,11 @@
 
 <script>
     export default {
+        data() {
+            return {
+                'name' : ''
+            }
+        },
         mounted() {
             console.log('Component mounted.')
         }
--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -1,32 +1,6 @@
 @component('layouts.app')
 
-    <div class="col-md-8 col-md-offset-2">
-        <div class="panel panel-default">
-            <div class="panel-heading">Editar producto</div>
-            <div class="panel-body">
+    <example>
 
-                {!! Form::open(['route' => ['products.update', $product ], 'method' => 'PUT']) !!}
-
-                    {{ Form::label('name') }}
-                    {{ Form::text('name', $product->name ,[
-                        'class' => "form-control"
-                    ]) }}
-
-                    {{ Form::label('category') }}
-                    {{ Form::select('category_id', $categories,  null,[
-                        'class' => "form-control"
-                    ]) }}
-
-                    <div class="form-group">
-                        <div class="col-md-8 col-md-offset-4">
-                            <button type="submit" class="btn btn-primary">
-                                Actualizar producto
-                            </button>
-                        </div>
-                    </div>
-                {!! Form::close() !!}
-
-            </div>
-        </div>
-    </div>
+    </example>
 @endcomponent
